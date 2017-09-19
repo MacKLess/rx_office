8b4fdcc7430ecdafeb9493f9bdd66f260d42d109
LDMacKrell <ldmackrell@gmail.com>
2017-09-19 10:06:38 -0700

    update app to pass .all test.

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -3,10 +3,22 @@ class Doctor
 
   def initialize(attributes)
     @name = attributes.fetch(:name)
-    # @speciality = attributes.fetch(:speciality)
+    @speciality = attributes.fetch(:speciality)
   end
+
+    def self.all
+      returned_doctors = DB.exec("SELECT * FROM doctors;")
+      doctors = []
+      returned_doctors.each do |doctor|
+        name = name.fetch('name')
+        speciality = speciality.fetch('speciality')
+        doctors.push(Doctor.new({:name => name, :speciality => speciality}))
+      end
+      doctors
+    end
 
     def ==(another_doctor)
       self.name.==(another_doctor.name)
     end
+
 end
--- spec/doctor_spec.rb
+++ spec/doctor_spec.rb
@@ -11,13 +11,18 @@ RSpec.configure do |config|
 end
 
 describe(Doctor) do
-  describe('#==') do
-    it('is the same doctor if it has the same name') do
-      doctor1 = Doctor.new({:name => 'Dr. Zhivago'})
-      doctor2 = Doctor.new({:name => 'Dr. Zhivago'})
-      expect(doctor1).to(eq(doctor2))
+  describe('.all') do
+    it('is empty at first') do
+      expect(Doctor.all()).to(eq([]))
     end
   end
 
+    describe('#==') do
+      it('is the same doctor if it has the same name') do
+        doctor1 = Doctor.new({:name => 'Dr. Zhivago', :speciality => "Russian method"})
+        doctor2 = Doctor.new({:name => 'Dr. Zhivago', :speciality => "Russian method"})
+        expect(doctor1).to(eq(doctor2))
+      end
+    end
 
 end
