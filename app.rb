f822d5c96c1c5fcc6222fa6d9cb5dc2e13dea05e
LDMacKrell <ldmackrell@gmail.com>
2017-09-19 16:45:04 -0700

    create UI pages WIP.

--- app.rb
+++ app.rb
@@ -3,5 +3,17 @@ require('sinatra-contrib')
 also_reload('lib/**/*.rb')
 require('./lib/doctor')
 require('pg')
+require('pry')
 
 DB = PG.connect({:dbname => "rx_office"})
+
+get('/') do
+  erb(:dr_roster)
+end
+
+post('/') do
+  name = params.fetch('name')
+  doctor = Doctor.new({:name => name, :speciality => speciality, :id => id})
+  doctor.save
+  erb(:dr_roster) #not sure about this
+end
